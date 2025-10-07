9d1e46fbcd6216ff8c27e3a09f55858ff0697c26
Kabilan M <142797492+Kabilan2005@users.noreply.github.com>
2025-10-07 06:53:32 +0530

    Delete citymate/__pycache__ directory

--- citymate/__pycache__/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    ROLE_CHOICES = (
-        ('user', 'User'),
-        ('admin', 'Admin'),
-    )
-    role = models.CharField( max_length=10,choices=ROLE_CHOICES,default='user')
